6c0693dda55c0ee0b46658d6b460b2ece8fd2135
Samkelo-ds <lebosdladla@gmail.com>
2025-07-22 07:04:13 +0000

    commit

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,18 +1,30 @@
-from utils import access_nested_map
-from parameterized import parameterized
+#!/usr/bin/env python3
+"""Unit tests for utils.access_nested_map"""
+
 import unittest
+from utils import access_nested_map
+
 
 class TestAccessNestedMap(unittest.TestCase):
-    """Test cases for access_nested_map."""
+    """Test cases for the access_nested_map function"""
 
-    @parameterized.expand([
-        ({}, ("a",)),
-        ({"a": 1}, ("a", "b")),
-    ])
-    def access_nested_map(nested_map, path):
-    """Access value in nested map using path tuple."""
-    for key in path:
-        nested_map = nested_map[key]
-    return nested_map
+    def test_access_nested_map(self):
+        """Test normal access"""
+        self.assertEqual(access_nested_map({"a": 1}, ("a",)), 1)
+        self.assertEqual(access_nested_map({"a": {"b": 2}}, ("a",)), {"b": 2})
+        self.assertEqual(access_nested_map({"a": {"b": 2}}, ("a", "b")), 2)
+
+    def test_access_nested_map_exception(self):
+        """Test access with missing keys"""
+        with self.assertRaises(KeyError):
+            access_nested_map({}, ("a",))
+
+        with self.assertRaises(KeyError):
+            access_nested_map({"a": 1}, ("a", "b"))
+
+
+if __name__ == '__main__':
+    unittest.main()
+
 
 
