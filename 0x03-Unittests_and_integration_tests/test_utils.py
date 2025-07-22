ba45e7f1f5864f9d24742b043ec36649f8110279
Samkelo-ds <lebosdladla@gmail.com>
2025-07-22 19:00:12 +0000

    Parameterize a unit test

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -2,7 +2,7 @@
 import unittest
 from unittest.mock import patch
 from parameterized import parameterized
-
+from utils import access_nested_map 
 from client import GithubOrgClient  # Adjust import path as per your project structure
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -15,8 +15,17 @@ class TestAccessNestedMap(unittest.TestCase):
     ])
     def test_access_nested_map(self, nested_map, path, expected):
         """Test that access_nested_map returns the expected value."""
-        from utils import access_nested_map  # Adjust import path as per your project structure
+         # Adjust import path as per your project structure
         self.assertEqual(access_nested_map(nested_map, path), expected)
+
+class TestAccessNestedMap(unittest.TestCase):
+    @parameterized.expand([
+        ({}, ("a",), KeyError),
+        ({"a": 1}, ("a", "b"), KeyError),
+     ])
+    def test_access_nested_map_exception(self, nested_map, path, expected):
+        self.assertRaiseException(test_access_nested_map(nested_map, path), expected):
+
  
 
 class TestGithubOrgClient(unittest.TestCase):
